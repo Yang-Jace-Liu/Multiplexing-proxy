c40691d92951a5e9ac86b43baacd4e5013d51ab6
Y. Jace Liu <yang.jace.liu@linux.com>
2019-03-06 17:07:24 -0700

    Server and client could do simple communications
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- proxy-server/src/main/java/jace/website/personal/Main.java
+++ proxy-server/src/main/java/jace/website/personal/Main.java
@@ -4,10 +4,9 @@ package jace.website.personal;
  * Hello world!
  *
  */
-public class App 
+public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
