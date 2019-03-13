986458569897d98846e71e4ed82ef176026c5995
Yang Liu <ly950710@gmail.com>
2019-03-12 22:20:01 -0600

    Convert to non-blocking sockets
    Signed-off-by: Yang Liu <ly950710@gmail.com>

--- proxy-client/src/main/java/jace/website/personal/Client.java
+++ proxy-client/src/main/java/jace/website/personal/Client.java
@@ -3,54 +3,96 @@ package jace.website.personal;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Client {
     private Config config = null;
-    private ServerSocket serverSocket;
-    private List<Socket> remoteSockets;
+    private List<SocketChannel> remoteSocketChannels;
+    private ByteBuffer buffer = ByteBuffer.allocate(10240);
 
     private static final Logger logger = LogManager.getLogger(Client.class);
 
     public Client(Config config) {
         this.config = config;
-        remoteSockets = new ArrayList<>();
+        remoteSocketChannels = new ArrayList<>();
     }
 
-    public void handshake() {
-        Socket socket;
+
+    public void Start() {
+        logger.debug("Trying to connect to remote port: " + config.getRemotePort());
         try {
-            socket = new Socket(config.getRemoteIp(), config.getRemotePort());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            DataInputStream in = new DataInputStream(socket.getInputStream());
+            InetSocketAddress address = new InetSocketAddress(InetAddress.getByName("localhost"), this.config.getRemotePort());
+            Selector selector = Selector.open();
 
-            String line = "Maximum connections: " + config.getNumberOfConnections();
-            out.writeUTF(line);
+            SocketChannel sc = SocketChannel.open();
+            sc.configureBlocking(false);
+            sc.connect(address);
+            sc.register(selector, SelectionKey.OP_CONNECT | SelectionKey.OP_READ | SelectionKey.OP_WRITE);
 
-            remoteSockets.add(socket);
-
-            int numberOfNewConnections = in.readInt();
-            for (int i = 0; i < numberOfNewConnections; i++) {
-                remoteSockets.add(new Socket(config.getRemoteIp(), config.getRemotePort()));
+            while (true) {
+                if (selector.select() > 0) {
+                    processReadySet(selector.selectedKeys());
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void Start() {
-        logger.debug("Client started on port: " + config.getRemotePort());
+    private void processReadySet(Set<SelectionKey> readySet) {
+        Iterator<SelectionKey> iterator = readySet.iterator();
+
+        while (iterator.hasNext()) {
+            SelectionKey key = iterator.next();
+            iterator.remove();
+
+            if (key.isConnectable()) processConnection(key);
+            if (key.isReadable()) processRead(key);
+        }
+    }
+
+    private void processConnection(SelectionKey key) {
+        SocketChannel sc = (SocketChannel) key.channel();
         try {
-            serverSocket = new ServerSocket(config.getServerPort());
-            handshake();
+            while (sc.isConnectionPending()) {
+                sc.finishConnect();
+                logger.debug("Connected to remote");
+            }
+        } catch (IOException e) {
+            key.cancel();
+            e.printStackTrace();
+        }
+    }
+
+    private void processRead(SelectionKey key) {
+        SocketChannel sc = (SocketChannel) key.channel();
+        try {
+            int length = sc.read(buffer);
+            if (length <= 0){
+                logger.debug("Remote closed the connection");
+                sc.close();
+                return;
+            }
+            String result = new String(buffer.array()).trim();
+            System.out.println("Message received from Server");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void processHandshake(SelectionKey key) {
+
     }
 }
--- proxy-server/src/main/java/jace/website/personal/Server.java
+++ proxy-server/src/main/java/jace/website/personal/Server.java
@@ -6,61 +6,78 @@ import org.apache.logging.log4j.Logger;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class Server {
     private int port;
     private int numberOfConnections;
     private static final Logger logger = LogManager.getLogger(Server.class);
-    private ServerSocket serverSocket;
-    private Map<Integer, Socket> sockets;
+    private ServerSocketChannel serverSocketChannel;
+    private Map<String, Integer> ip2id;
 
-    public Server (Config config) {
+    public Server(Config config) {
         this.port = config.getPort();
         this.numberOfConnections = config.getNumberOfConnections();
 
-        sockets = new HashMap<>();
-    }
-
-    public void handshake(Socket socket) {
-        DataInputStream in = null;
-        try {
-            in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-            String hello = in.readUTF();
-            if (!hello.startsWith("Maximum connections: ")) {
-                socket.close();
-                return;
-            }
-            int remoteNumberOfConnections = Integer.valueOf(hello.split(":")[1].replace(" ", ""));
-            int resultNumberOfConnections = remoteNumberOfConnections > numberOfConnections ? numberOfConnections : remoteNumberOfConnections;
-            out.writeInt(resultNumberOfConnections);
-            int id = Data.getInstance().getNewId();
-            sockets.put(id, socket);
-            logger.debug("A client connected to server through port " + socket.getPort());
-
-            for (int i = 1; i < resultNumberOfConnections; i++) {
-                Socket anotherSocket = serverSocket.accept();
-                logger.debug("A client connected to server through port " + anotherSocket.getPort());
-                sockets.put(id, anotherSocket);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ip2id = new HashMap<>();
     }
 
     public void start() {
         logger.debug("Server started on port: " + port);
 
-        try{
-            serverSocket = new ServerSocket(port);
+        try {
+            Selector selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.bind(new InetSocketAddress("0.0.0.0", port));
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-            Socket socket = serverSocket.accept();
-            handshake(socket);
+            SelectionKey key = null;
+            while (true) {
+                if (selector.select() <= 0) continue;
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                while (iterator.hasNext()) {
+                    key = iterator.next();
+                    iterator.remove();
+
+                    if (key.isAcceptable()) {
+                        SocketChannel sc = serverSocketChannel.accept();
+                        sc.configureBlocking(false);
+                        sc.register(selector, SelectionKey.OP_READ);
+
+                        String remoteIp = sc.getRemoteAddress().toString().split(":")[0].replace("/", "");
+                        if (!ip2id.containsKey(remoteIp))
+                            ip2id.put(remoteIp, Data.getInstance().getNewId());
+                        logger.debug("Connection Accepted: " + sc.getRemoteAddress());
+                    }
+
+                    if (key.isReadable()) {
+                        SocketChannel sc = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(10240);
+                        int length = sc.read(buffer);
+                        if (length <= 0) {
+                            logger.debug("Connection Closed: " + sc.getRemoteAddress());
+                            sc.close();
+                        } else {
+                            String result = new String(buffer.array()).trim();
+                            System.out.println("Message received: " + result + " length: " + result.length());
+                        }
+                    }
+                }
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
